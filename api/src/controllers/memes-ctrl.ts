204df436ebe8f9bf80f1be72a72b2f51b62c4742
Jaka Bernard <jaka.bernard@kalmia.si>
2020-11-08 07:53:03 +0100

    Cursor pointer

--- api/src/controllers/memes-ctrl.ts
+++ api/src/controllers/memes-ctrl.ts
@@ -50,7 +50,9 @@ export class MemesController extends Controller {
         fill: red;
         position: relative;
         top: 5px;
-        width: 50px;" viewBox="0 0 32 29.6">
+        width: 50px;
+        cursor: pointer;
+        " viewBox="0 0 32 29.6">
         <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
         c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
       </svg> `
