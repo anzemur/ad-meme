70dd31f51ecfbdf3e1c80174bc99698b8da827a7
Anze Mur <am3158@student.uni-lj.si>
2020-11-08 04:26:42 +0100

    Added addon popup.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,8 +1,27 @@
-var client = new XMLHttpRequest();
-client.open('GET', '/CatFacts.txt');
-client.onreadystatechange = function() {
-    var facts_str = client.responseText;
-    var facts = facts_str.split("\n");
-	document.getElementById("CatFact").textContent = facts[Math.floor(Math.random() * (facts.length))];
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
 }
-client.send();
+
+
+var client = new HttpClient();
+client.get('http://localhost:3000/api/v1/memes', function(response) {
+    try {
+        response = JSON.parse(response)
+        for (var i = 0; i < response.length; i++) {
+            console.log(response[i])
+            $('#topMemes').append('<div class="img-wrapper"><div class="likes">' + response[i].likes + ' likes</div><img class="meme-image" src="' + response[i].imageUrl + '"/></div>');
+        }
+    } catch (error) {
+        console.log(error);
+    }    
+    
+});
