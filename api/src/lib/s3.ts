91aa7ab230045b917c0f21067f5a2abe9cb0e72f
Anze Mur <am3158@student.uni-lj.si>
2020-11-08 09:01:16 +0100

    fixes

--- api/src/lib/s3.ts
+++ api/src/lib/s3.ts
@@ -21,7 +21,7 @@ function getS3Client() {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_KEY,
     },
-    signatureVersion: 'v3',
+    signatureVersion: 'v2',
   });
 }
 
--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -6,8 +6,31 @@ var HttpClient = function() {
                 aCallback(anHttpRequest.responseText);
         }
 
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
+        anHttpRequest.open("GET", aUrl, true);            
+        anHttpRequest.send(null);
+    }
+
+    this.post = function(aUrl, body, aCallback) {
+      var anHttpRequest = new XMLHttpRequest();
+      anHttpRequest.onreadystatechange = function() { 
+        if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+            aCallback(anHttpRequest.responseText);
+      }
+
+      anHttpRequest.open("POST", aUrl, true);            
+      anHttpRequest.send(body);
+    }
+
+
+    this.put = function(aUrl, body, aCallback) {
+      var anHttpRequest = new XMLHttpRequest();
+      anHttpRequest.onreadystatechange = function() { 
+        if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+            aCallback(anHttpRequest.responseText);
+      }
+
+      anHttpRequest.open("PUT", aUrl, true);            
+      anHttpRequest.send(body);
     }
 }
 
