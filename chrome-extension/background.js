5db0c64b3e6c9bd5e8e0dcb0de269d029091b3b9
Anze Mur <am3158@student.uni-lj.si>
2020-11-08 08:11:28 +0100

    Fixes

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -11,6 +11,63 @@ var HttpClient = function() {
     }
 }
 
+$(':file').on('change', function () {
+    var file = this.files[0];
+  
+    if (file.size > 1024) {
+      alert('max upload size is 1k');
+    }
+});
+
+$(':button').on('click', function () {
+
+    var client = new HttpClient();
+    client.get('http://localhost:3000/api/v1/file-upload/signed-url', function(response) {
+        try {
+            response = JSON.parse(response)
+            for (var i = 0; i < response.length; i++) {
+                console.log(response[i])
+                $('#topMemes').append('<div class="img-wrapper"><div class="likes">' + response[i].likes + ' likes</div><img class="meme-image" src="' + response[i].imageUrl + '"/></div>');
+            }
+        } catch (error) {
+            console.log(error);
+        }    
+        
+    });
+
+
+    $.ajax({
+      // Your server script to process the upload
+      url: 'http://localhost:3000/api/v1',
+      type: 'POST',
+  
+      // Form data
+      data: new FormData($('form')[0]),
+  
+      // Tell jQuery not to process data or worry about content-type
+      // You *must* include these options!
+      cache: false,
+      contentType: false,
+      processData: false,
+  
+      // Custom XMLHttpRequest
+      xhr: function () {
+        var myXhr = $.ajaxSettings.xhr();
+        if (myXhr.upload) {
+          // For handling the progress of the upload
+          myXhr.upload.addEventListener('progress', function (e) {
+            if (e.lengthComputable) {
+              $('progress').attr({
+                value: e.loaded,
+                max: e.total,
+              });
+            }
+          }, false);
+        }
+        return myXhr;
+      }
+    });
+  });
 
 var client = new HttpClient();
 client.get('http://localhost:3000/api/v1/memes', function(response) {
@@ -25,3 +82,5 @@ client.get('http://localhost:3000/api/v1/memes', function(response) {
     }    
     
 });
+
+
